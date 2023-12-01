0ce12f69c2b6a6a400e91d90d740913860bf3bc6
samiullah51 <samii51728@gmail.com>
2023-12-01 15:24:07 +0500

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,147 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import { useEffect, useState } from "react";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io("http://localhost:3001");
 
 function App() {
-  //Room State
-  const [room, setRoom] = useState("");
-
-  // Messages States
   const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [editMessage, setEditMessage] = useState({ id: null, newText: "" });
 
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
+  useEffect(() => {
+    // Fetch all messages when the component mounts
+    fetchMessages();
+
+    // Listen for new messages from the server
+    socket.on("receive_message", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    // Listen for deleted messages from the server
+    socket.on("delete_message", (data) => {
+      setMessages(messages.filter((msg) => msg._id !== data.id));
+    });
+
+    // Listen for edited messages from the server
+    socket.on("edit_message", (data) => {
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg._id === data._id ? { ...msg, text: data.text } : msg
+        )
+      );
+    });
+
+    return () => {
+      socket.off("receive_message");
+      socket.off("delete_message");
+      socket.off("edit_message");
+    };
+  }, [messages]);
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/api/get_messages");
+      const data = await response.json();
+      setMessages(data);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
     }
   };
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room });
+  const sendMessage = async () => {
+    try {
+      await fetch("http://localhost:3001/api/send_message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: message }),
+      });
+
+      setMessage("");
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   };
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
+  const deleteMessage = async (id) => {
+    try {
+      await fetch(`http://localhost:3001/api/delete_message/${id}`, {
+        method: "DELETE",
+      });
+
+      // Broadcast delete message event to the server
+      socket.emit("delete_message", { id });
+    } catch (error) {
+      console.error("Error deleting message:", error);
+    }
+  };
+
+  const startEditing = (id, text) => {
+    setEditMessage({ id, newText: text });
+  };
+
+  const cancelEditing = () => {
+    setEditMessage({ id: null, newText: "" });
+  };
+
+  const editMessageText = async () => {
+    try {
+      await fetch(`http://localhost:3001/api/edit_message/${editMessage.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ newText: editMessage.newText }),
+      });
+
+      // Broadcast edit message event to the server
+      socket.emit("edit_message", {
+        id: editMessage.id,
+        newText: editMessage.newText,
+      });
+
+      setEditMessage({ id: null, newText: "" });
+    } catch (error) {
+      console.error("Error editing message:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
+    <div>
+      <div>
+        {messages.map((msg) => (
+          <div key={msg._id}>
+            {msg.text}{" "}
+            <button onClick={() => deleteMessage(msg._id)}>Delete</button>{" "}
+            <button onClick={() => startEditing(msg._id, msg.text)}>
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
+      {editMessage.id !== null ? (
+        <div>
+          <input
+            type="text"
+            value={editMessage.newText}
+            onChange={(e) =>
+              setEditMessage({ ...editMessage, newText: e.target.value })
+            }
+          />
+          <button onClick={editMessageText}>Save</button>
+          <button onClick={cancelEditing}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(cors());
-
+app.use(express.json());
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -15,16 +16,123 @@ const io = new Server(server, {
   },
 });
 
+// MongoDB connection setup
+mongoose.connect(
+  "mongodb+srv://developerjs:WebDev0!a@cluster0.m7zxglh.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const messageSchema = new mongoose.Schema({
+  text: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
+// API endpoint to send a message
+app.post("/api/send_message", async (req, res) => {
+  const { text } = req.body;
+
+  // Save message to MongoDB
+  const message = new Message({ text });
+  await message.save();
+
+  // Broadcast the message to all connected clients
+  io.emit("receive_message", { text });
+
+  res.status(200).send({ success: true, message: "Message sent successfully" });
+});
+
+// API endpoint to retrieve all messages
+app.get("/api/get_messages", async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.status(200).send(messages);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ success: false, message: "Error retrieving messages" });
+  }
+});
+
+// API endpoint to delete a message
+app.delete("/api/delete_message/:id", async (req, res) => {
+  const messageId = req.params.id;
+
+  try {
+    // Delete the message from MongoDB
+    await Message.findByIdAndDelete(messageId);
+
+    // Broadcast the deleted message ID to all connected clients
+    io.emit("delete_message", { id: messageId });
+
+    res
+      .status(200)
+      .send({ success: true, message: "Message deleted successfully" });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error deleting message" });
+  }
+});
+app.put("/api/edit_message/:id", async (req, res) => {
+  const messageId = req.params.id;
+  const { newText } = req.body;
+
+  try {
+    // Update the message in MongoDB
+    const updatedMessage = await Message.findByIdAndUpdate(
+      messageId,
+      { text: newText },
+      { new: true }
+    );
+
+    // Broadcast the updated message to all connected clients
+    io.emit("edit_message", updatedMessage);
+
+    res
+      .status(200)
+      .send({ success: true, message: "Message edited successfully" });
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Error editing message" });
+  }
+});
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
+  // Listen for edit message events
+  socket.on("edit_message", async (data) => {
+    try {
+      // Update the message in MongoDB
+      const updatedMessage = await Message.findByIdAndUpdate(
+        data.id,
+        { text: data.newText },
+        { new: true }
+      );
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+      // Broadcast the updated message to all connected clients
+      io.emit("edit_message", updatedMessage);
+    } catch (error) {
+      console.error("Error editing message:", error);
+    }
   });
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // Listen for delete message events
+  socket.on("delete_message", async (data) => {
+    try {
+      // Delete the message from MongoDB
+      await Message.findByIdAndDelete(data.id);
+    } catch (error) {
+      console.error("Error deleting message:", error);
+    }
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 });
 
 server.listen(3001, () => {
